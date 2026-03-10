8382c514cec7c247f1d5d66fba3f466423c67c1a
DhirajUpadhyay <dhirajupadhyay5277@gmail.com>
2026-03-10 21:34:32 +0530

    Update useCurrencyinfo.js

--- src/components/useCurrencyinfo.js
+++ src/components/useCurrencyinfo.js
@@ -3,11 +3,11 @@ import {useEffect, useState} from "react"
 
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
-    useEffect(() => {
-        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
-        .then((res) => res.json())
-        .then((res) => setData(res.rates))
-        .catch((error) => console.error("Error fetching currency data:", error))
+ useEffect(() => {
+        fetch(`https://api.frankfurter.dev/v1/latest?from=${currency}`)
+            .then((res) => res.json())
+            .then((res) => setData(res.rates))
+            .catch((error) => console.error("Error fetching currency data:", error))
     }, [currency])
     console.log(data);
     return data
